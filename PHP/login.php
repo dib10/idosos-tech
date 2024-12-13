802746770a347d5322b65e4c9362d37798f49d24
Caio <129826533+dib10@users.noreply.github.com>
2024-12-13 12:22:00 -0300

    Update login.php

--- PHP/login.php
+++ PHP/login.php
@@ -19,7 +19,7 @@
     <div class="row justify-content-center">
         <div class="col-md-6">
             <h2 class="text-center mb-4">Login</h2>
-            <form action="processar_login.php" method="POST">
+            <form action="processar_Login.php" method="POST">
                 <div class="mb-3 position-relative">
                     <label for="email" class="form-label">Email</label>
                     <div class="input-group">
