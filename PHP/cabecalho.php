75f112b853d45e70cf5f59897130a0bf2ae071ef
Pedro Lauton <129104265+PedroLauton@users.noreply.github.com>
2024-12-13 14:31:44 -0300

    Update cabecalho.php

--- PHP/cabecalho.php
+++ PHP/cabecalho.php
@@ -23,9 +23,6 @@
                 <li class="nav-item">
                     <a class="nav-link text-white" href="#">Contatos</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="login.php" title="Ir para tela de login">Login</a> 
-                </li>
             </ul>
         </div>
     </div>
