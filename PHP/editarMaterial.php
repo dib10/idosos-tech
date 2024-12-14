9df6657882f216dca367a484de14fba441a218ee
Pedro Lauton <129104265+PedroLauton@users.noreply.github.com>
2024-12-14 13:32:56 -0300

    Update editarMaterial.php

--- PHP/editarMaterial.php
+++ PHP/editarMaterial.php
@@ -50,6 +50,7 @@
                 <label for="ano" class="form-label">Ano:</label>
                 <select id="ano" name="ano" class="form-select" required>
                     <option value="">Selecione o ano</option>
+                    <option value="2025" <?= $material['ano'] == 2025 ? 'selected' : '' ?>>2025</option>
                     <option value="2024" <?= $material['ano'] == 2024 ? 'selected' : '' ?>>2024</option>
                     <option value="2023" <?= $material['ano'] == 2023 ? 'selected' : '' ?>>2023</option>
                     <option value="2022" <?= $material['ano'] == 2022 ? 'selected' : '' ?>>2022</option>
