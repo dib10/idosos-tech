92f64cce88869e7e30bcfc672c1ec148e8466443
Pedro Lauton <129104265+PedroLauton@users.noreply.github.com>
2024-12-14 13:32:26 -0300

    Update adicionarMaterial.php

--- PHP/adicionarMaterial.php
+++ PHP/adicionarMaterial.php
@@ -32,6 +32,7 @@
                 <label for="ano" class="form-label">Ano:</label>
                 <select id="ano" name="ano" class="form-select" required>
                     <option value="">Selecione o ano</option>
+                    <option value="2025">2025</option>
                     <option value="2024">2024</option>
                     <option value="2023">2023</option>
                     <option value="2022">2022</option>
