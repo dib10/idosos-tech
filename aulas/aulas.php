e2d2710a146eeb66c1a120ea0936d6b4568d4287
Pedro Lauton <129104265+PedroLauton@users.noreply.github.com>
2024-12-14 00:45:33 -0300

    Update aulas.php

--- aulas/aulas.php
+++ aulas/aulas.php
@@ -99,56 +99,56 @@
 <br>
 
 <!-- Seção com botões -->
-<section class="additional-sections text-center" aria-labelledby="titulo-secoes">
-    <?php if (!empty($row['link_tarefa']) || !empty($row['link_jogo']) || !empty($row['link_video'])): ?>
-    <h1 class="mt-5" id="titulo-aula">Outras seções para ver!</h1>
-    <?php endif; ?>
-    <br>
-    <div class="row justify-content-center m-5">
-        <?php if (!empty($row['link_tarefa'])): ?>
-        <div class="col-12 col-md-4 mb-4">
-            <div class="card h-100">
-                <a href="<?= $row['link_tarefa']?>" class="text-decoration-none">
-                    <img src="../img/tarefa.png" alt="Ícone de tarefas da aula" class="button-image card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">Tarefas da aula</h5>
-                        <p class="card-text">Clique aqui!</p>
-                    </div>
-                </a>
+<?php if (!empty($row['link_tarefa']) || !empty($row['link_jogo']) || !empty($row['link_video'])): ?>
+    <section class="additional-sections text-center" aria-labelledby="titulo-secoes">
+        <?php if (!empty($row['link_tarefa']) || !empty($row['link_jogo']) || !empty($row['link_video'])): ?>
+        <h1 class="mt-5" id="titulo-aula">Outras seções para ver!</h1>
+        <br>
+        <div class="row justify-content-center m-5">
+            <?php if (!empty($row['link_tarefa'])): ?>
+            <div class="col-12 col-md-4 mb-4">
+                <div class="card h-100">
+                    <a href="<?= $row['link_tarefa']?>" class="text-decoration-none">
+                        <img src="../img/tarefa.png" alt="Ícone de tarefas da aula" class="button-image card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Tarefas da aula</h5>
+                            <p class="card-text">Clique aqui!</p>
+                        </div>
+                    </a>
+                </div>
             </div>
-        </div>
-        <?php endif; ?>
-
-        <?php if (!empty($row['link_video'])): ?>
-        <div class="col-12 col-md-4 mb-4">
-            <div class="card h-100">
-                <a href="<?= $row['link_video']?>" class="text-decoration-none">
-                    <img src="../img/gravacao-de-video.png" alt="Ícone de gravação de aula" class="button-image card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">Gravação da aula</h5>
-                        <p class="card-text">Clique aqui!</p>
-                    </div>
-                </a>
+            <?php endif; ?>
+    
+            <?php if (!empty($row['link_video'])): ?>
+            <div class="col-12 col-md-4 mb-4">
+                <div class="card h-100">
+                    <a href="<?= $row['link_video']?>" class="text-decoration-none">
+                        <img src="../img/gravacao-de-video.png" alt="Ícone de gravação de aula" class="button-image card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Gravação da aula</h5>
+                            <p class="card-text">Clique aqui!</p>
+                        </div>
+                    </a>
+                </div>
             </div>
-        </div>
-        <?php endif; ?>
-
-        <?php if (!empty($row['link_jogo'])): ?>
-        <div class="col-12 col-md-4 mb-4">
-            <div class="card h-100">
-                <a href="<?= $row['link_jogo']?>" class="text-decoration-none">
-                    <img src="../img/jogo.png" alt="Ícone de jogo da aula" class="button-image card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">Jogo da aula</h5>
-                        <p class="card-text">Clique aqui!</p>
-                    </div>
-                </a>
+            <?php endif; ?>
+    
+            <?php if (!empty($row['link_jogo'])): ?>
+            <div class="col-12 col-md-4 mb-4">
+                <div class="card h-100">
+                    <a href="<?= $row['link_jogo']?>" class="text-decoration-none">
+                        <img src="../img/jogo.png" alt="Ícone de jogo da aula" class="button-image card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Jogo da aula</h5>
+                            <p class="card-text">Clique aqui!</p>
+                        </div>
+                    </a>
+                </div>
             </div>
+            <?php endif; ?>
         </div>
-        <?php endif; ?>
-    </div>
-</section>
-
+    </section>
+<?php endif; ?>
 <!-- Seta antes da seção de contatos -->
 <div class="text-center mt-5">
     <div class="arrow" aria-hidden="true">&#8595;</div>
